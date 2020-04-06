a8ff361eb9e5558fbdd7966c8b2c781c9fe7604a
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-06 23:11:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // add solution here
+
+function theBeatlesPlay(musicians , instruments){
+
+let newArr = [];
+
+for (let i = 0 ; i < musicians.length ; i++){
+
+newArr.push(`${musicians[i]} plays ${instruments[i]}`);
+}
+return newArr;
+}
+
+
+function iLoveTheBeatles(number){
+  
+  
+  let arr= [];
+ 
+ do{
+   
+  arr.push("I love the Beatles!" );
+number +=1;
+ }while(number<15);
+   
+ 
+  return arr;
+}
+
+
+
+
+function johnLennonFacts (factsAboutJohn){
+  
+   var i = 0;
+  
+  let arr = [];
+  
+  while (i < factsAboutJohn.length){
+    
+    arr.push(`${factsAboutJohn[i]}!!!`);
+    
+    i++;
+  }
+  return arr;
+
+}
